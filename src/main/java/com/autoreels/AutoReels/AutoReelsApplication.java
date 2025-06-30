2dc61be958ad1c13c15f1bedb85b0913e4f38f33
Tuyen572004 <htkt004@gmail.com>
2025-07-01 00:49:48 +0700

    modify publised and video

--- src/main/java/com/autoreels/AutoReels/AutoReelsApplication.java
+++ src/main/java/com/autoreels/AutoReels/AutoReelsApplication.java
@@ -1,11 +1,11 @@
-package com.windowprogramming.ClothingStoreManager;
+package com.autoreels.AutoReels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClothingStoreManagerApplication {
+public class AutoReelsApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ClothingStoreManagerApplication.class, args);
+		SpringApplication.run(AutoReelsApplication.class, args);
 	}
 }
